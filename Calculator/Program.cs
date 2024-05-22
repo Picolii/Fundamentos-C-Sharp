f3e2a4b11c96f30e5d99d6bf85912f1988a99fe7
Bernardo Picoli <bernardo.picoli@glok.com.br>
2024-05-22 11:25:06 -0300

    Create and config archives

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(valor1);
+        }
+    }
+}
