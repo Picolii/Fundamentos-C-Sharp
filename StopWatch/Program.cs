7c8e44ee7944e2f1f4df788fd29b58d3de85d7cd
Bernardo Picoli <bernardo.picoli@glok.com.br>
2024-05-23 10:58:54 -0300

    Substring and take option

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -24,7 +24,12 @@ namespace StopWatch
             Console.WriteLine("How long do you want to count?");
 
             string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
             Console.WriteLine(data);
+            Console.WriteLine(type);
+            Console.WriteLine(time);
         }
 
         static void StartChronometer(int time)
