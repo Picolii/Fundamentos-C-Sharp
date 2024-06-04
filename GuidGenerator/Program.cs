1596149d914049349264626a59d10db3dddb8364
Bernardo Picoli <bernardo.picoli@glok.com.br>
2024-06-04 09:47:39 -0300

    Guid generator

--- GuidGenerator/Program.cs
+++ GuidGenerator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuidGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Press 'R' to generate a NEW GUID!");
+            while (Console.ReadKey(true).Key == ConsoleKey.R)
+            {
+                Guid newGuid = Guid.NewGuid();
+                Console.WriteLine(newGuid.ToString().ToUpper().Replace('r', ' '));
+
+            }
+        }
+    }
+}
