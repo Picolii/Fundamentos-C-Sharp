dd3f2db9b77d6a2f7d94984f92d16417b42e2300
Bernardo Picoli <bernardo.picoli@glok.com.br>
2024-06-04 15:20:12 -0300

    Guid Generator remake

--- GuidGenerator/Program.cs
+++ GuidGenerator/Program.cs
@@ -1,19 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace GuidGenerator
+﻿namespace GuidGenerator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Clear();
-            Console.WriteLine("Press 'R' to generate a NEW GUID!");
-            while (Console.ReadKey(true).Key == ConsoleKey.R)
-            {
-                Guid newGuid = Guid.NewGuid();
-                Console.WriteLine(newGuid.ToString().ToUpper().Replace('r', ' '));
+            Console.WriteLine("Press 'R' to generate a NEW GUID! \nPress 'C' to console clear! \n");
 
-            }
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.R)
+                {
+                    Guid newGuid = Guid.NewGuid();
+                    Console.WriteLine(newGuid.ToString().ToUpper());
+                }
+                else if (keyInfo.Key == ConsoleKey.C)
+                {
+                    Console.WriteLine("\nCleaning, please wait!");
+                    Thread.Sleep(700);
+                    Console.WriteLine("Done!");
+                    Thread.Sleep(500);
+                    Main();
+                }
+
+            } while (keyInfo.Key != ConsoleKey.Escape);
+
+            Console.WriteLine("Trying to finish! Press key again!");
         }
     }
 }
